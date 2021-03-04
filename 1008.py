3d9d7062c13d0a567d4f891b8748eec79e995703
Eveneto <40215962+Eveneto@users.noreply.github.com>
2021-03-04 13:03:59 -0300

    Create 1008.py

---
+++ 1008.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+numero = int(input())
+horas = int(input()) 
+salariohoras = float(input())  
+
+salario = horas * salariohoras  
+
+print(f'NUMBER = {numero}')
+print(f'SALARY = U$ {salario:.2f}')
